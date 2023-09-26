d24cce4d6dba6aea8a22fc3a4db535c4eee174a2
Edgar Uriel <80359378+xEdgarosx@users.noreply.github.com>
2023-09-26 00:30:22 -0700

    Update load_folder.py

--- load_folder.py
+++ load_folder.py
@@ -1,6 +1,7 @@
 import os
 import redis
 import re
+from bs4 import BeautifulSoup
 # https://github.com/redis/redis-py
 
 r = redis.Redis(host='localhost', port=6379, db=0)
@@ -14,8 +15,19 @@ def load_folder(path):
         if match:
             with open(path + file) as f:
                 html = f.read()
-                r.set(match.group(1), html)
-            print(match.group(0), match.group(1))
+                book_id = match.group(1)
+                create_index(book_id, html)
+                r.set(book_id, html)
+            print(match.group(0), book_id)
+
+def create_index(book_id, html):
+    soup = BeautifulSoup(html, 'html.parser')
+    tag = str(soup.p).lower()
+    palabras = tag.split()
+    for pal in palabras:
+        pal = pal.replace(",","")
+        r.sadd(pal, book_id)
 
 
 load_folder('html/books/')
+
